258302016ed7bc0c6387bcaa3b30a558a4c6cecf
Aries-PC <ariesekoprasetyo@ydrive.pro>
2023-05-12 20:55:15 +0700

    [MODIFY] Connection DB & env

--- db/connection.go
+++ db/connection.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"Assigment_6/initializers"
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
@@ -11,9 +12,17 @@ import (
 var DB *gorm.DB
 
 func ConnectToDb() error {
-	initializers.LoadEnv()
 	var err error
-	dsn := os.Getenv("DB_URL")
+	initializers.LoadEnv()
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_SSL_MODE"),
+	)
+
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed To Connect")
